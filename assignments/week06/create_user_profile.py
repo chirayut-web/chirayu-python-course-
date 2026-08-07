5261ac80624f82f35471d1a04504fda2bd916a36
DDDubper <85053623+Yang-cmy@users.noreply.github.com>
2026-08-07 12:15:11 +0700

    new

--- assignments/week06/create_user_profile.py
+++ assignments/week06/create_user_profile.py
@@ -1,9 +1,15 @@
-def create_user_profile(username, age, premium):
-    user_profile = (f"[{username}] (age: [{age}]) - [{premium}]")
+def create_user_profile(username, age, premium = False):
+
+    user_type = "standard_user"
+
+    if premium == True:
+        user_type = "premium_user"
+
+    user_profile = (f"[{username}] (age: [{age}]) - [{user_type}]")
     return user_profile
 
 
 
-print(create_user_profile("Bonchoo",41,"premium User"))
-print(create_user_profile("Yang",19,"Standard User"))
-print(create_user_profile("Poom",22,"Standard User"))
+print(create_user_profile("Bonchoo",41))
+print(create_user_profile("Yang",19))
+print(create_user_profile("Poom",22,True))
--- labs/week06-lab/example/geometry.py
+++ labs/week06-lab/example/geometry.py
@@ -1,4 +1,6 @@
+"""
 def calculation_rectangle_area(length, widgth):
     # Calculate_rectangle_area
     area = length * widgth
     print(f"Rectangle with length" {length} )
+"""
