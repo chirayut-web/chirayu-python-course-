3d12df9f24e095eb99a1951ecf501a8fb1a79a3a
DDDubper <chirayu.t@ku.th>
2026-09-25 11:52:07 +0700

    Week7-Quiz1-ClassRecandCircle

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -12,13 +12,33 @@ class Rectangle:
 
     # Method to get the area
     def get_area(self):
+        return self.length * self.width
         pass
 
     # Method to get the perimeter
     def get_perimeter(self):
+        return 2 * (self.length + self.width)
+        pass
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    # Method to get the area
+    def get_area(self):
+        return 22/7 * self.radius * self.radius
+        pass
+
+    # Method to get the perimeter
+    def get_perimeter(self):
+        return 2 * 22/7 * self.radius
         pass
 
 
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+print("")
+cir = Circle(7)
+print(cir.get_area())       # Should print 50
+print(cir.get_perimeter())  # Should print 30
